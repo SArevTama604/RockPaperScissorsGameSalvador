882b2ade354cb1f7bd5f5f7d47f47f0302c52d2e
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-16 22:17:56 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RockPaperScissorsGameSalvador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
